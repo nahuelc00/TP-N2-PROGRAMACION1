487308b5107d0095c2fec7791956acb0a31943e3
Nahuel Lorenzo <nahuel.lorenzodev@gmail.com>
2021-12-28 20:24:39 -0300

    finales-de-programa

--- script-para-librerias.cmd
+++
@@ -1,6 +0,0 @@
-gcc -c main.c -o main.o
-gcc -c validaciones.c -o validaciones.o
-gcc -c grafico.c -o grafico.o
-gcc -c ordenar.c -o ordenar.o
-gcc -c abm.c -o abm.o
-gcc validaciones.o abm.o ordenar.o grafico.o main.o -o programa.exe
